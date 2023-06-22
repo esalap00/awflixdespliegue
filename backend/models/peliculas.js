14bff35f5655297763fd891efb21d785f3a5dd20
esalap00 <100129524+esalap00@users.noreply.github.com>
2023-06-22 20:47:52 +0200

    Update peliculas.js

--- backend/models/peliculas.js
+++ backend/models/peliculas.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
@@ -34,41 +34,5 @@ let peliculasSchema = new Schema({
 })
 
 peliculas = mongoose.model("peliculas", peliculasSchema);
-=======
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
 
-let peliculasSchema = new Schema({
-
-    foto: { 
-        type: String,
-        required: true,
-    },
-
-    titulo: {
-        type: String,
-        required: true,
-        unique: true,
-    },
-
-    genero: {
-        type: String,
-        required: true,
-    },
-
-    sinopsis: {
-        type: String,
-        required: true,
-    },
-
-    //Mirar solo pueda ser hasta 10
-    valoracion: {
-        type: Number,
-        required: true,
-    },
-
-})
-
-peliculas = mongoose.model("peliculas", peliculasSchema);
->>>>>>> 2d0301e71493fdda225a2c12ae651d8033dce78b
 module.exports = {peliculas, peliculasSchema}
