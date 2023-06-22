334d190f5ebbd1c7f2404279b03afee7f8982f98
esalap00 <100129524+esalap00@users.noreply.github.com>
2023-06-22 20:50:11 +0200

    Update contacto.js

--- backend/models/contacto.js
+++ backend/models/contacto.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
@@ -32,39 +31,4 @@ let contactoSchema = new Schema({
 })
 
 contacto = mongoose.model("contacto", contactoSchema);
-=======
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
-
-let contactoSchema = new Schema({
-
-    idUser: {
-        type: Object,
-        required: true,
-    },
-
-    email: {
-        type: String,
-        required: true,
-    },
-
-    asunto: {
-        type: String,
-        required: true,
-    },
-
-    cuerpo: {
-        type: String,
-        required: true,
-    },
-
-    estado: {
-        type: String,
-        required: true,
-    }
-
-})
-
-contacto = mongoose.model("contacto", contactoSchema);
->>>>>>> 2d0301e71493fdda225a2c12ae651d8033dce78b
 module.exports = {contacto, contactoSchema}
