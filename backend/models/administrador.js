b643fbc4b41a94fc095b506488aaabb6bfa1cd89
esalap00 <100129524+esalap00@users.noreply.github.com>
2023-06-22 20:50:34 +0200

    Update administrador.js

--- backend/models/administrador.js
+++ backend/models/administrador.js
@@ -15,23 +15,4 @@ let adminSchema = new Schema({
 })
 
 admin = mongoose.model("admin", adminSchema)
-=======
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
-
-let adminSchema = new Schema({
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-    },
-
-    contrasena: {
-        type: String,
-        required: true,
-    }
-})
-
-admin = mongoose.model("admin", adminSchema)
->>>>>>> 2d0301e71493fdda225a2c12ae651d8033dce78b
 module.exports = {admin, adminSchema}
