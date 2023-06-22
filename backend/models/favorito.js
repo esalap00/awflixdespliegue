865f9f6c631814f24cd8aeff0eee272e3c520482
esalap00 <100129524+esalap00@users.noreply.github.com>
2023-06-22 20:49:46 +0200

    Update favorito.js

--- backend/models/favorito.js
+++ backend/models/favorito.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
@@ -17,24 +17,4 @@ let favoritosSchema = new Schema({
 })
 
 favorito = mongoose.model("favorito", favoritosSchema);
-=======
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
-
-let favoritosSchema = new Schema({
-    
-    idUser: {
-        type: Object,
-        required: true,
-    },
-
-    idPelicula: {
-        type: Object,
-        required: true,
-    },
-
-})
-
-favorito = mongoose.model("favorito", favoritosSchema);
->>>>>>> 2d0301e71493fdda225a2c12ae651d8033dce78b
 module.exports = {favorito, favoritosSchema}
