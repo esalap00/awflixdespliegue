bcc857612528b3bfaf9e10cb5b1622a9123cdc24
esalap00 <100129524+esalap00@users.noreply.github.com>
2023-06-22 20:49:10 +0200

    Update visualizaciones.js

--- backend/models/visualizaciones.js
+++ backend/models/visualizaciones.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
@@ -16,25 +16,9 @@ let visualizacionesSchema = new Schema({
     
 })
 
-visualizaciones = mongoose.model("visualizaciones", visualizacionesSchema);
-=======
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
 
-let visualizacionesSchema = new Schema({
 
-    idUser: {
-        type: Object,
-        required: true,
-    },
-
-    idPelicula: {
-        type: Object,
-        required: true,
-    },
-    
-})
 
 visualizaciones = mongoose.model("visualizaciones", visualizacionesSchema);
->>>>>>> 2d0301e71493fdda225a2c12ae651d8033dce78b
+
 module.exports = {visualizaciones, visualizacionesSchema}
