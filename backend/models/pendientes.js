d7a9296ff48c4aed7797bf3a51bbf40007526e8e
esalap00 <100129524+esalap00@users.noreply.github.com>
2023-06-22 20:53:40 +0200

    Update pendientes.js

--- backend/models/pendientes.js
+++ backend/models/pendientes.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
@@ -17,24 +17,4 @@ let pendientesSchema = new Schema({
 })
 
 pendientes = mongoose.model("pendientes", pendientesSchema);
-=======
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
-
-let pendientesSchema = new Schema({
-
-    idUser: {
-        type: Object,
-        required: true,
-    },
-
-    idPelicula: {
-        type: Object,
-        required: true,
-    },
-
-})
-
-pendientes = mongoose.model("pendientes", pendientesSchema);
->>>>>>> 2d0301e71493fdda225a2c12ae651d8033dce78b
 module.exports = {pendientes, pendientesSchema}
